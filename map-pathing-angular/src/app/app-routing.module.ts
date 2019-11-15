7084839802102588664917b6118da07105e573b0
Rafferty Hutari Askara <rafferty.askara@gmail.com>
2019-11-15 11:58:34 -0800

    experimenting gmap and mock API - need to modify readme

--- map-pathing-angular/src/app/app-routing.module.ts
+++ map-pathing-angular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './components/map/map.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MapComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- map-pathing-angular/src/app/app.module.ts
+++ map-pathing-angular/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+
+import gmapAPIKey from '../../config/application.config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AgmCoreModule.forRoot({
+      apiKey: gmapAPIKey,
+      libraries: ['places'],
+      language: 'en'
+    }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
